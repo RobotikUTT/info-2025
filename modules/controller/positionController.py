ba229a95b58a17d3739cf7ec6ed25ffcbf20bd09
Guipy314 <pyguillaume3105@gmail.com>
2024-12-10 19:03:28 +0100

    Clean but not tested position controller

--- modules/controller/positionController.py
+++ modules/controller/positionController.py
@@ -1,18 +1,19 @@
 from simple_pid import PID
 from modules.position_tracker.position_tracker import PositionTracker
-from threading import Thread
-from threading import Lock
+from threading import Thread, Lock
 from utils.config import Config
 from utils.log import Log
 import math
 from modules.communication.speed_communication import SpeedCommunication
 import time
 
-class positionController:
-    def __int__(self):
+
+class PositionController:
+    def __init__(self):
         self.config = Config().get()
         self.log = Log("position_controller")
 
+        # PID Controllers
         p_pos = self.config["PID_position"]["P"]
         i_pos = self.config["PID_position"]["I"]
         d_pos = self.config["PID_position"]["D"]
@@ -24,6 +25,7 @@ class positionController:
         d_angle = self.config["PID_angle"]["D"]
         self.pid_w = PID(p_angle, i_angle, d_angle)
 
+        # Position tracker and communication
         self.positionTracker = PositionTracker()
         self.speedCommunication = SpeedCommunication()
 
@@ -35,55 +37,64 @@ class positionController:
         self.target_speed = self.config["speed"]
         self.target_acceleration = self.config["acceleration"]
 
-        controllerLoop = Thread(self.run())
         self.lock = Lock()
-        controllerLoop.start()
+        self.running = True
+
+        # Start controller loop
+        self.controllerLoop = Thread(target=self.run)
+        self.controllerLoop.start()
 
     def goTo(self, x, y, w):
-        self.targetPosX = x
-        self.targetPosY = y
-        self.targetPosW = w
         with self.lock:
-            self.pid_x.setpoint(x)
-            self.pid_y.setpoint(y)
-            self.pid_w.setpoint(w)
+            self.targetPosX = x
+            self.targetPosY = y
+            self.targetPosW = w
+            self.pid_x.setpoint = x
+            self.pid_y.setpoint = y
+            self.pid_w.setpoint = w
 
     def run(self):
-
+        current_speed_x = 0
+        current_speed_y = 0
+        current_speed_w = 0
         pre_time = time.time()
 
+        while self.running:
+            try:
+                delta_time = time.time() - pre_time
+                pre_time = time.time()
 
-        while True:
-            delta_time = pre_time - time.time()
-            pre_time = time.time()
+                current_pos_x, current_pos_y, current_pos_w = self.positionTracker.getCurrentPosition()
 
-            current_pos_x, current_pos_y, current_pos_w = self.positionTracker.getCurrentPosition()
-            with self.lock:
-                control_x = self.pid_x(current_pos_x)
-                control_y = self.pid_y(current_pos_y)
-                control_w = self.pid_w(current_pos_w)
+                with self.lock:
+                    control_x = self.pid_x(current_pos_x)
+                    control_y = self.pid_y(current_pos_y)
+                    control_w = self.pid_w(current_pos_w)
 
-            pid_speed = math.sqrt(control_x**2 + control_y**2)
+                pid_speed = math.sqrt(control_x**2 + control_y**2)
 
-            if pid_speed > self.target_speed:
+                if pid_speed > self.target_speed:
+                    current_speed = math.sqrt(current_speed_x**2 + current_speed_y**2)
+                    if current_speed < self.target_speed:
+                        current_speed += self.target_acceleration * delta_time
 
-                current_speed = math.sqrt(current_x**2 + current_y**2)
+                    d_x, d_y = self.normalize((self.targetPosX - current_pos_x, self.targetPosY - current_pos_y))
 
-                if not current_speed > self.target_speed:
-                    # acceleration
-                    current_speed = current_speed + self.target_acceleration * delta_time
+                    current_speed_x = current_speed * d_x
+                    current_speed_y = current_speed * d_y
+                else:
+                    current_speed_x = control_x
+                    current_speed_y = control_y
 
-                    direction_x = current_speed * (self.targetPosX - current_x)/
-                    direction_y = current_speed
+                current_speed_w = control_w
 
-                    current_x = current_speed *
+                self.speedCommunication.sendSpeedCart(current_speed_x, current_speed_y, current_speed_w)
+            except Exception as e:
+                self.log.error(f"Error in run loop: {e}")
 
-
-            else:
-                pass
-
-
-
-
-
-
+    def normalize(self, vect):
+        x, y = vect
+        d = math.sqrt(x**2 + y**2)
+        if d == 0:
+            return 0, 0
+        return x / d, y / d
