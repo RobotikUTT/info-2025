f43cd526fc2abc4fb438cbc88bf2162ed814463f
Guipy314 <pyguillaume3105@gmail.com>
2024-12-18 19:40:52 +0100

    Continue implementation of real simulation

--- modules/slam/position_tracker.py
+++ modules/slam/position_tracker.py
@@ -1,3 +1,8 @@
+import sys
+import time
+import qwiic_otos
+
+
 class PositionTracker:
     _instance = None
 
@@ -20,3 +25,65 @@ class PositionTracker:
 
     def setCurrentPosition(self, position):
         self.position = position
+
+
+def runExample():
+    print("\nQwiic OTOS Example 1 - Basic Readings\n")
+
+    # Create instance of device
+    myOtos = qwiic_otos.QwiicOTOS()
+
+    # Check if it's connected
+    if not myOtos.is_connected():
+        print("The device isn't connected to the system. Please check your connection", file=sys.stderr)
+        return
+
+    # Initialize the device
+    myOtos.begin()
+
+    print("Ensure the OTOS is flat and stationary during calibration!")
+    for i in range(5, 0, -1):
+        print(f"Calibrating in {i} seconds...")
+        time.sleep(1)
+
+    print("Calibrating IMU...")
+
+    # Calibrate the IMU, which removes the accelerometer and gyroscope offsets
+    myOtos.calibrateImu()
+
+    # Reset the tracking algorithm
+    myOtos.resetTracking()
+
+    # Create PositionTracker instance (singleton)
+    position_tracker = PositionTracker()
+
+    # Main loop
+    while True:
+        # Get the latest position, which includes the x and y coordinates, plus
+        # the heading angle
+        myPosition = myOtos.getPosition()
+
+        # Update position in the PositionTracker singleton
+        position_tracker.setCurrentPosition((myPosition.x, myPosition.y, myPosition.h))
+
+        # Print measurement
+        print()
+        print("Position:")
+        print(f"X (Inches): {myPosition.x}")
+        print(f"Y (Inches): {myPosition.y}")
+        print(f"Heading (Degrees): {myPosition.h}")
+
+        # Wait a bit to prevent spamming the serial port
+        time.sleep(0.5)
+
+        # Optionally, you can print continuously using the following code:
+        # print(f"{myPosition.x}\t{myPosition.y}\t{myPosition.h}")
+        # time.sleep(0.01)
+
+
+if __name__ == '__main__':
+    try:
+        runExample()
+    except (KeyboardInterrupt, SystemExit) as exErr:
+        print("\nEnding Example")
+        sys.exit(0)
