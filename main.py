fdc3a899a79c0b85da30c2962cb05c6607d61046
Guipy314 <pyguillaume3105@gmail.com>
2024-12-18 10:27:02 +0100

    Continue implementation of real simulation

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
-from modules import *
-
+from modules.communication.speed_communication import SpeedCommunication
+import time
+from modules.communication.i2c_communication import I2CCommunication
 def main():
-    # test of position control
-    pass
+    i2c_comm = I2CCommunication("esp_steppers")
+    while True:
+        i2c_comm.write("Hello world!")
+        time.sleep(0.1)
+        # SpeedCommunication.sendSpeedPolar(0.2, 0.2, 0.0)
+
+
+
 
 
 if __name__ == "__main__":
