6f96ec1df48e294f42a7cd0855aec3ea0d6a193f
Guipy314 <pyguillaume3105@gmail.com>
2025-01-29 12:23:39 +0100

    circle test

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from modules.communication.speed_communication import SpeedCommunication
 import time
 from modules.communication.i2c_communication import I2CCommunication
-import math
+from math import cos, sin, pi
 
 def main():
     speed_comm = SpeedCommunication()
-    x = 0
+    rotation_speed = 0.03
+    angle = 0
+    radius = 10
     while True:
-        x += 1
+        angle = (angle + rotation_speed) % (2 * pi)
+        p_x = cos(angle) * radius
+        p_y = sin(angle) * radius
+        v_x = -p_y * rotation_speed
+        v_y = p_x * rotation_speed
         time.sleep(0.1)
-        speed_comm.sendSpeedCart(math.cos(x)*0.2, 0.0, 0.0)
+        speed_comm.sendSpeedCart(v_x, v_y, 0.0)
 
 
 
