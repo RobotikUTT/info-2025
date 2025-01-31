f7715d71fe30b5083e9236dcb1dce269f24d0714
Guipy314 <pyguillaume3105@gmail.com>
2025-01-31 16:24:06 +0100

    added path_following.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from modules.communication.speed_communication import SpeedCommunication
 import time
 from math import cos, sin, pi
 from modules.lidar.lidar import LidarService, DetectionService, PrinterService
+from modules.navigation.path_following import PathFollower
 
 def main():
     lidar_service = LidarService()
@@ -9,19 +10,8 @@ def main():
     detection_service = DetectionService(200)
     printer_service = PrinterService()
     lidar_service.observers += [detection_service]
-    speed_comm = SpeedCommunication(detection_service)
-    rotation_speed = 0.1
-    angle = 0
-    radius = 4
-    while True:
-        if not detection_service.stop:
-            angle = (angle + rotation_speed) % (2 * pi)
-            p_x = cos(angle) * radius
-            p_y = sin(angle) * radius
-            v_x = -p_y * rotation_speed
-            v_y = p_x * rotation_speed
-            time.sleep(0.1)
-            speed_comm.sendSpeedCart(v_x, v_y, 0.0)
+    path_follower = PathFollower(detection_service)
+    path_follower.start()
 
 if __name__ == "__main__":
     main()
