2d34efd959ea701eac3fe726a3f269283b267994
k8pl3r-sh <k8pl3r@gmail.com>
2025-04-06 11:39:41 +0200

    argparse to option to run motors or not

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-
+import argparse
 from modules.lidar.lidar import LidarService, DetectionService, PrinterService
 from modules.navigation.path_following import PathFollower
 
-def main():
+def main(run_path_follower: bool):
     lidar_service = LidarService()
     lidar_service.start()
+
     detection_service = DetectionService()
     printer_service = PrinterService()
-    lidar_service.observers += [detection_service]
-    path_follower = PathFollower(detection_service) # permet de faire le dessin en secours
-    # faudrait le combiner avec le PID quand même, chemin dans un json
 
-    path_follower.start()
+    lidar_service.observers += [detection_service]
+
+    if run_path_follower:
+        path_follower = PathFollower(detection_service)  # permet de faire le dessin en secours
+        path_follower.start()  # faudrait le combiner avec le PID quand même, chemin dans un json
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Start the Lidar system.")
+    parser.add_argument(
+        '--run',
+        action='store_true',
+        help='Start the PathFollower after Lidar initialization'
+    )
+    args = parser.parse_args()
+
+    main(run_path_follower=args.run)
