5ebad146b59124994a6bdab0a08dc89d56c56650
Guillaume Py <pyguillaume3105@gmail.com>
2025-05-30 01:55:13 +0200

    added raw test stepper

---
+++ tests/raw_stepper.test.py
@@ -0,0 +1,10 @@
+from modules.communication.speed_communication import SpeedCommunication
+from time import sleep
+
+def main():
+    s_com = SpeedCommunication()
+    s_com.sendSpeedCart(0.1, 0.1, 0);
+    sleep(2)
+
+if __name__ == "__main__":
+    main()
